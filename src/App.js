e0c6c94624cc5001fdca954d34a1340d6befc6fb
Anikroy7 <roy645800@gmail.com>
2022-04-01 17:51:02 +0600

    routes added and navbar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import OrderReview from './Components/OrderRiview/OrderReview';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/orderreview' element={<OrderReview></OrderReview>}></Route>
+      </Routes>
     </div >
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to Cool-TShirt-Zone</h1>
+            <nav className='navbar'>
+                <Link to={'/home'}>Home</Link>
+                <Link to={'/orderreview'}>Order Review</Link>
+            </nav>
         </div>
     );
 };
